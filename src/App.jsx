f2f75e5dbb5deec6d11c061fa149ae19f1ec869d
cristian-corbalan <ccorbalandw@gmail.com>
2023-12-30 16:11:38 -0300

    Add UserInput and Results components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    return (
+        <>
+            <UserInput></UserInput>
+            <Results></Results>
+        </>
+    )
 }
 
 export default App
@@ -10,8 +16,8 @@ export default App
 /*
 * TODO List:
 * Utilice el proyecto inicial adjunto a esta lección
-* TODO: Añada componentes para mostrar una cabecera, recoger los datos introducidos por el usuario y generar la tabla de resultados.
-* Obtener y almacenar los datos introducidos por el usuario (es decir, los parámetros de inversión introducidos).
+* Añada componentes para mostrar una cabecera, recoger los datos introducidos por el usuario y generar la tabla de resultados.
+* TODO: Obtener y almacenar los datos introducidos por el usuario (es decir, los parámetros de inversión introducidos).
 * Derivar los resultados de la inversión con la ayuda de la función de utilidad proporcionada (en el proyecto inicial)
 * Mostrar los resultados de la inversión en una tabla HTML (Utilice <table>, <thead>, <tbody> <tr>, <th>, <td>)
 * Mostrar condicionalmente un mensaje informativo si se ha introducido una duración no válida (< 1)
