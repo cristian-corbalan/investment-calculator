50de0841b2de935f4e76b7082c0040c577ca3cb8
cristian-corbalan <ccorbalandw@gmail.com>
2023-12-30 16:10:19 -0300

    Add state and event to input value

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,21 @@
-export default function Input ({id, label, ...props}) {
+import {useState} from "react";
+
+export default function Input ({id, label, initialValue = 0, ...props}) {
+    const [value, setValue] = useState(initialValue);
+
+    function handleValueChange(event) {
+        setValue(+event.target.value);
+    }
+
     return (
         <div>
             <label htmlFor={id}>{label}</label>
-            <input id={id} {...props}/>
+            <input
+                id={id}
+                value={value}
+                onChange={handleValueChange}
+                {...props}
+            />
         </div>
     )
 }
