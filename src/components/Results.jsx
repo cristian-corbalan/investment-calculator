5dfee7763d43de97f024ae0fac31ab28384cc6d6
cristian-corbalan <ccorbalandw@gmail.com>
2023-12-30 15:59:22 -0300

    Create Results component

---
+++ src/components/Results.jsx
@@ -0,0 +1,24 @@
+export default function Results() {
+    return (
+        <table id="result">
+            <thead className="center">
+            <tr>
+                <th>Year</th>
+                <th>Investment Value</th>
+                <th>Interest (Year)</th>
+                <th>Total Interest</th>
+                <th>Inverted Capital</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
