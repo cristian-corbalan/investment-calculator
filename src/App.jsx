ec9d6f777376c4ce80caeb88b58155225a1b21ef
cristian-corbalan <ccorbalandw@gmail.com>
2023-12-30 16:15:28 -0300

    Create state for investment values

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import {useState} from "react";
 import UserInput from "./components/UserInput.jsx";
 import Results from "./components/Results.jsx";
+import {calculateInvestmentResults} from "./util/investment.js";
+
+const INVESTMENT_VALUES = {
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+}
 
 function App() {
+    const [investmentValues, setInvestmentValues] = useState(INVESTMENT_VALUES);
+
     return (
         <>
             <UserInput></UserInput>
